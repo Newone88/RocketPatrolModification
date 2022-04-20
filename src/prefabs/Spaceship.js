937ffd70df6a01ee903b6c35a6012651b72f8ce0
Newone88 <81717053+Newone88@users.noreply.github.com>
2022-04-19 21:18:23 -0700

    Added All the assets and messed with collision

--- src/prefabs/Spaceship.js
+++ src/prefabs/Spaceship.js
@@ -1,16 +1,29 @@
 //Spaceship's PreFabs
 class Spaceship extends Phaser.GameObjects.Sprite {
-    constructor(scene, x, y, texture, frame, pointValue) {
+    constructor(scene, x, y, texture, frame, pointValue,isCap) {
         super(scene,x,y, texture, frame);
-        this.anims.play('test');  
+
+        if(isCap == 1){
+            this.anims.play('cap');
+        }
+        else{
+            this.anims.play('test'); 
+        }
         scene.add.existing(this);                         // Add to Existing Scene
+        this.captain = isCap;
         this.points = pointValue;                         // Store PointValue
-        this.moveSpeed = game.settings.spaceshipSpeed;    // Movement in Pixels per Frame
+        if(isCap == 1){
+            this.moveSpeed = game.settings.spaceshipSpeed+5;
+        }
+        else{
+            this.moveSpeed = game.settings.spaceshipSpeed;    // Movement in Pixels per Frame
+        }
     }
 
     update() {
-        //Move Spaceship Left
+        //Moves Knight left
         this.x -= this.moveSpeed;
+        //If knight goes off screen 
         //Wrap Around From left edge to right edge
         if(this.x <= 0 - this.width){
             this.x = game.config.width;
